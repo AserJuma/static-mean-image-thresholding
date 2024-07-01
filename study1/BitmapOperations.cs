070bf30e4454420ca0d5bd8ddf89460dfe3789bb
Aser Juma <aserjuma@protonmail.com>
2024-07-01 10:22:59 +0300

    Some fixes/amendments

--- study1/BitmapOperations.cs
+++ study1/BitmapOperations.cs
@@ -6,28 +6,6 @@ namespace study1
 {
     public static class BitmapOperations
     {
-        public static bool Convert2GrayScaleFast(Bitmap bmp) // Not a true conversion, treats it as a 24bit rgb image with all equal values.
-        {
-            BitmapData bmData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height),
-                    ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
-            unsafe
-            {
-                byte* p = (byte*)(void*)bmData.Scan0.ToPointer();
-                int stopAddress = (int)p + bmData.Stride * bmData.Height;
-                
-                while ((int)p != stopAddress)
-                {
-                    p[0] = (byte)(p[0] * .114 + p[1] * .587 + p[2] * .299);
-                    p[1] = p[0];
-                    p[2] = p[0];
-                    p += 3;
-                }
-            }
-            bmp.UnlockBits(bmData);
-            return true;
-        }
-        
-        
         // Otsu helper function, Computes image histogram of pixel intensities
         // Initializes an array, iterates through and fills up histogram count values
         private static unsafe void GetHistogram(byte* pt, int width, int height, int stride, int[] histArr)
@@ -52,7 +30,7 @@ namespace study1
             for (i = init; i <= end; i++)
                 sum += hist[i];
 
-            return (float)sum;
+            return sum;
         }
         
         // Otsu helper function, Get the mean values in the equation
@@ -64,7 +42,7 @@ namespace study1
             for (i = init; i <= end; i++)
                 sum += i * hist[i];
 
-            return (float)sum;
+            return sum;
         }
 
         // Otsu helper function, Maximum element
@@ -87,77 +65,64 @@ namespace study1
         // Otsu's threshold
         private static int GetOtsuThreshold(Bitmap bmp)
         {
-            byte threshold = 0;
-	        float[] vet = new float[256];
+            float[] vet = new float[256];
             int[] hist = new int[256];
             vet.Initialize();
 
-	        float p1, p2, p12;
-	        int k;
-
             BitmapData bmData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height),
-            ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+            ImageLockMode.ReadOnly, bmp.PixelFormat);
             unsafe
             {
-                byte* p = (byte*)(void*)bmData.Scan0.ToPointer();
-
+                byte* p = (byte*)bmData.Scan0.ToPointer();
                 GetHistogram(p,bmp.Width,bmp.Height,bmData.Stride, hist); // Fills up an array with pixel intensity values
-
-                // loop through all possible threshold values and maximize between class variance
-                for (k = 1; k != 255; k++)
+                // loop through all possible threshold values and maximize between-class variance
+                for (int k = 1; k != 255; k++)
                 {
-                    p1 = Px(0, k, hist); // foreground
-                    p2 = Px(k + 1, 255, hist); // background
+                    var p1 = Px(0, k, hist);
+                    var p2 = Px(k + 1, 255, hist);
                     // Continually sums up histogram values in different ranges, covering the span of the image data, in two float values p1, p2
-                    p12 = p1 * p2; // product of probabilities p1, p2
+                    var p12 = p1 * p2;
                     if (p12 == 0) 
                         p12 = 1;
                     float diff = (Mx(0, k, hist) * p2) - (Mx(k + 1, 255, hist) * p1);
-                    
-                    vet[k] = (float)diff * diff / p12; // Computes and stores variance values for each threshold value using simple variance formula from statistics.
+                    vet[k] = diff * diff / p12; // Computes and stores variance values for each threshold value using simple variance formula from statistics.
                     //vet[k] = (float)Math.Pow((Mx(0, k, hist) * p2) - (Mx(k + 1, 255, hist) * p1), 2) / p12; // Another way to compute variance (more overhead/overly complex)
                 }
             }
             bmp.UnlockBits(bmData);
 
-            threshold = (byte)FindMax(vet, 256); // Finds maximum variance value
-
-            return threshold;
+            return (byte)FindMax(vet, 256); // Finds maximum variance value
         }
         
-        public static void ApplyThreshold(Bitmap bmp, int thresholdValue)
+        public static Bitmap ApplyThreshold(Bitmap bmp, int thresholdValue)
         {
-            var maxVal = 256;
-            if (thresholdValue < 0) return;
-            else if (thresholdValue >= maxVal) return;
-            var bmpData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb); // 8bit grayscale: Format8bppIndexed
+            const int maxVal = 256;
+            if (thresholdValue >= maxVal) return null;
+            
+            var bmpData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadWrite, bmp.PixelFormat);
+            int size = (bmpData.Stride * bmp.Height);
+            var counter = 0;
             unsafe
             {
-                var ptr = (byte*)bmpData.Scan0.ToPointer();
-                var stopAddress = (int)ptr + bmpData.Stride * bmpData.Height;
-
-                while ((int)ptr != stopAddress)
+                var pt = (byte*)bmpData.Scan0.ToPointer();
+                while (counter < size)
                 {
-                    //var totalRgb = (ptr[0] + ptr[1] + ptr[2]) / 3;
-                    if (ptr[0] <= thresholdValue) // Since they are all the same values in a 24bit format, it's enough to check the first value (ptr[0])
-                    { 
-                        ptr[0] = 0; ptr[1] = 0; ptr[2] = 0; 
-                    }
-                    else
-                    {
-                        ptr[0] = 255; ptr[1] = 255; ptr[2] = 255;
-                    }
-                    ptr += 3;
+                    *pt = (byte)(*pt <= thresholdValue ? 0 : 255);
+                    pt += 1;
+                    counter++;
                 }
             }
             bmp.UnlockBits(bmpData);
+            return bmp;
         }
-        // Runs Otsu's method group
         public static int GetOptimalThreshold(Bitmap bmp)
         {
-            var threshold = GetOtsuThreshold(bmp);
-            // Console.WriteLine("Optimal Threshold: " + thresh);
-            return (threshold);
+            if (bmp.Height == 0 || bmp.Width == 0) return (-1);
+            return GetOtsuThreshold(bmp);
         }
     }
 }
+
+
+// TODO : Check Bitmap data before processing. OK
+// TODO : Return a new bitmap after processing. OK
---
+++ study1/Gui.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace study1
+{
+    public class Gui : Form
+    {
+        //Bitmap
+        private Bitmap _sourceBitmap;
+        private Bitmap _clonedBitmap;
+        private Bitmap _retBitmap;
+        //Main form items
+        private MainMenu _myMainMenu;
+        private MenuItem _menuItem1;
+        private MenuItem _menuItem2;
+        //In menuItem1:
+        private MenuItem _fileLoad;
+        private MenuItem _fileExit;
+        //In menuItem2:
+        private MenuItem _staticThreshold;
+        private MenuItem _meanThreshold;
+        
+        private readonly System.ComponentModel.Container _components = null;
+
+        private bool _thresholdApplied;
+        private Gui()
+        {
+            InitializeComponent();
+            _sourceBitmap = new Bitmap(2, 2);
+        }
+
+        
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing) _components?.Dispose();
+            base.Dispose(disposing);
+        }
+        
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics gg = e.Graphics;
+            if (!_thresholdApplied)
+            {
+                gg.DrawImage(_sourceBitmap, new Rectangle
+                (this.AutoScrollPosition.X, this.AutoScrollPosition.Y, 
+                    (_sourceBitmap.Width), (_sourceBitmap.Height)
+                ));
+            }
+            else
+            {
+                this.ClientSize = new Size((_sourceBitmap.Width * 2), (_sourceBitmap.Height));
+                
+                gg.DrawImage(_sourceBitmap, new Rectangle
+                (this.AutoScrollPosition.X, this.AutoScrollPosition.Y, 
+                    (_sourceBitmap.Width), (_sourceBitmap.Height)
+                ));
+                gg.DrawImage(_clonedBitmap, new Rectangle
+                (this.AutoScrollPosition.X + (_sourceBitmap.Width), this.AutoScrollPosition.Y, 
+                    (_clonedBitmap.Width), (_clonedBitmap.Height)
+                ));
+            }
+        }
+
+        private void InitializeComponent()
+        {
+            // Inits the main menu, it's tabs, then their child tabs
+            this._myMainMenu = new MainMenu();
+            this._menuItem1 = new MenuItem();
+            this._menuItem2 = new MenuItem();
+            
+            this._fileLoad = new MenuItem();
+            this._fileExit = new MenuItem();
+            
+            this._staticThreshold = new MenuItem();
+            this._meanThreshold = new MenuItem();
+            
+            //Main Menu Tabs
+            this._myMainMenu.MenuItems.AddRange(new[]
+            {
+                this._menuItem1,
+                this._menuItem2,
+            });
+
+            // First Dropdown Menu Tab
+            this._menuItem1.Text = "File";
+            this._menuItem1.Index = 0;
+            this._menuItem1.MenuItems.AddRange(new []
+            {
+            this._fileLoad,
+            this._fileExit,
+                });
+            this._fileLoad.Index = 0;
+            this._fileLoad.Shortcut = Shortcut.CtrlL;
+            this._fileLoad.Text = "Load";
+            this._fileLoad.Click += this.File_Load;
+
+            this._fileExit.Index = 1;
+            this._fileExit.Shortcut = Shortcut.CtrlE;
+            this._fileExit.Text = "Exit";
+            this._fileExit.Click += this.File_Exit;
+
+            // Second Dropdown Menu Tab
+            this._menuItem2.Text = "Binarization";
+            this._menuItem2.Index = 1;
+            this._menuItem2.MenuItems.AddRange(new []
+            {
+            this._staticThreshold,
+            this._meanThreshold,
+                });
+            this._staticThreshold.Index = 0;
+            this._staticThreshold.Text = "Static Threshold";
+            this._staticThreshold.Click += this._applyStaticThreshold;
+            
+            this._meanThreshold.Index = 1;
+            this._meanThreshold.Text = "Mean Threshold";
+            this._meanThreshold.Click += this._applyMeanThreshold;
+            
+            //Gui
+            this.AutoScaleBaseSize = new Size(5, 13);
+            this.BackColor = SystemColors.ControlLight;
+            this.ClientSize = new Size(350, 350);
+            this.Menu = this._myMainMenu;
+            this.Name = $"Study into .NET GUI & Image Processing";
+            this.Text = "Thresholding";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+        
+        private void File_Load(object sender, EventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "Bitmap files (*.bmp)|*.bmp|TIFF files (.tiff)|*.tiff|Jpeg files (*.jpg)|*.jpg|All valid files (*.bmp/.tiff/*.jpg)|*.bmp;*.tiff;*.jpg";
+            openFileDialog.FilterIndex = 4;
+            openFileDialog.RestoreDirectory = false;
+            if (DialogResult.OK != openFileDialog.ShowDialog()) return;
+            _sourceBitmap?.Dispose();
+            _sourceBitmap = (Bitmap)Image.FromFile(openFileDialog.FileName, false);
+            //if(_sourceBitmap.PixelFormat != PixelFormat.Format8bppIndexed) return; // ONLY Format8bppIndexed will currently load
+            this.ClientSize = new Size((_sourceBitmap.Width), (_sourceBitmap.Height));
+            _thresholdApplied = false; _clonedBitmap = null; // _destBitmap = _cloneBitmapFromSource(_sourceBitmap); 
+            this.AutoScroll = true;
+            this.Invalidate();
+        }
+        private void Save_Btmp(Bitmap b)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.InitialDirectory = "c:\\";
+            saveFileDialog.Filter = "Bitmap files (*.bmp)|*.bmp|Jpeg files (*.jpg)|*.jpg|All valid files (*.bmp/*.jpg)|*.bmp;*.jpg";
+            saveFileDialog.FilterIndex = 1;
+            saveFileDialog.RestoreDirectory = false;
+
+            if (DialogResult.OK == saveFileDialog.ShowDialog())
+            {
+                b.Save(saveFileDialog.FileName);
+            }
+        }
+        private void File_Exit(object sender, EventArgs e) { this.Close(); }
+
+        private static Bitmap _cloneBitmap(Bitmap bitmap)
+        {
+            return (Bitmap)bitmap.Clone();
+        }
+        
+        private void _applyStaticThreshold(object sender, EventArgs e)
+        {
+            var input = new Input();
+            input.Text = "Threshold";
+            if (DialogResult.OK != input.ShowDialog()) return;
+            _clonedBitmap = _cloneBitmap(_sourceBitmap);
+            _applyThresh(_clonedBitmap, input.Nvalue);
+        }
+        private void _applyMeanThreshold(object sender, EventArgs e)
+        {
+            int th = BitmapOperations.GetOptimalThreshold(_sourceBitmap);
+            _clonedBitmap = _cloneBitmap(_sourceBitmap);
+            _applyThresh(_clonedBitmap, th);
+            Console.WriteLine("Optimal Threshold Value: " + th);
+        }
+        //private Stopwatch _stopwatch = new Stopwatch();
+        private void _applyThresh(Bitmap b, int t)
+        {
+            //_stopwatch.Restart();
+            if (t < 0) return;
+            _retBitmap = BitmapOperations.ApplyThreshold(b, t);
+            //_stopwatch.Stop();
+            //Console.WriteLine($"Time Taken : {_stopwatch.ElapsedMilliseconds} ms");
+            _thresholdApplied = true;
+            this.Invalidate();
+            Save_Btmp(_retBitmap);
+        }
+        private static void Main()
+        {
+            Application.Run(new Gui());
+        }
+    }
+}
+
+// TODO: GUI ISSUE: Fix Static input prompt
+// TODO: GUI ISSUE: Fix window scaling issue when reloading to a smaller image than the previous
